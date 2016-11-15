5ea8ccfe99095df0afe7ec42ad12323266d43442
Karun Joseph <karun.joseph@aggiemail.usu.edu>
2016-11-15 11:12:59 -0700

    added initial figs

--- data/datapreproc.py
+++ data/datapreproc.py
@@ -16,12 +16,32 @@ us_all_data = pd.read_csv('CrimeStatebyState_all.csv', skiprows = 5)[:-6]
 us_all_data = us_all_data[['Year', 'Population', 'Violent crime total', 'Murder and nonnegligent Manslaughter', 'Forcible rape', 'Robbery', 'Aggravated assault', 'Property crime total', 'Burglary', 'Larceny-theft', 'Motor vehicle theft', 'Violent Crime rate', 'Murder and nonnegligent manslaughter rate', 'Forcible rape rate', 'Robbery rate', 'Aggravated assault rate', 'Property crime rate', 'Burglary rate', 'Larceny-theft rate', 'Motor vehicle theft rate']]
 
 #get column headers using: list(us_all_data.columns.values)
-plt.title('Time series of xyz crime')
+plt.title('Time series of Robbery rate across US')
 plt.xlabel('Year')
-plt.ylabel('xyz crime number')
-plt.plot(us_all_data['Year'], us_all_data['Violent crime total'])
+plt.ylabel('Robbery rate (offenses per 100,000 people')
+plt.plot(us_all_data['Year'], us_all_data['Robbery rate'])
 plt.show()
 
+#get column headers using: list(us_all_data.columns.values)
+plt.title('Time series of Aggravated assault rate across US')
+plt.xlabel('Year')
+plt.ylabel('Aggravated assault rate (offenses per 100,000 people')
+plt.plot(us_all_data['Year'], us_all_data['Aggravated assault rate'])
+plt.show()
+
+#list(utah_data.columns.values)
+plt.title('Time series of Robbery rate in Utah')
+plt.xlabel('Year')
+plt.ylabel('Robbery rate (offenses per 100,000 people')
+plt.plot(utah_data['Year'], utah_data['Robbery rate'])
+plt.show()
+
+#list(utah_data.columns.values)
+plt.title('Time series of Aggravated assault rate in Utah')
+plt.xlabel('Year')
+plt.ylabel('Aggravated assault rate (offenses per 100,000 people')
+plt.plot(utah_data['Year'], utah_data['Aggravated assault rate'])
+plt.show()
 
 #Utah counties specific data,cleaning
 #TODO
